f1fe06582be59dfb773e973b621da7c1fc76e2b3
bakeraw2 <mmichalskidaniel2@gmail.com>
2013-11-03 17:09:17 +0100

    dodanie kilku widoków oraz prezentera

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import view.main_menu.MainMenuFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Author: dmichalski
+ * Date: 01.11.13
+ */
+public class Main {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainMenuFrame mv = new MainMenuFrame();
+                mv.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                mv.setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/main/java/view/main_menu/MainMenuFrame.java
@@ -0,0 +1,63 @@
+package view.main_menu;
+
+import controller.MainMenuPresenter;
+import database.TextsDao;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Author: dmichalski
+ * Date: 01.11.13
+ */
+public class MainMenuFrame extends JFrame {
+    private MainMenuPresenter presenter;
+    private JMenuItem closeMenuItem;
+
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 300;
+
+    public MainMenuFrame() {
+        setUpFrame();
+        initializeComponents();
+    }
+
+    private void setUpFrame() {
+        // zmiana wyglądu na domyślny dla systemu
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ignored) {}
+        catch (InstantiationException ignored) {}
+        catch (IllegalAccessException ignored) {}
+        catch (UnsupportedLookAndFeelException ignored) {}
+
+        setTitle(TextsDao.get("view.MainView.title"));
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+    }
+
+    private void initializeComponents() {
+        presenter = new MainMenuPresenter();
+
+        MainMenuPanel mainPanel = new MainMenuPanel();
+        add(mainPanel, BorderLayout.CENTER);
+
+        JMenuBar menuBar = createJMenuBar();
+        setJMenuBar(menuBar);
+
+        presenter.setVisitsBtn(mainPanel.getRegisterBtn());
+        presenter.setCloseMenuItem(closeMenuItem);
+    }
+
+    private JMenuBar createJMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu(TextsDao.get("view.fileMenu"));
+        menuBar.add(fileMenu);
+
+        closeMenuItem = new JMenuItem(TextsDao.get("view.closeMenuItem"));
+        fileMenu.add(closeMenuItem);
+
+        return menuBar;
+    }
+}
