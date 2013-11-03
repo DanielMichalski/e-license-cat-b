ce0b474b4ff4c17f16c6436bdeea7a21e81bc0db
bakeraw2 <notarget1>
2013-11-03 22:36:19 +0100

    dodanie widoku egzaminu

--- src/main/java/util/Const.java
+++ src/main/java/util/Const.java
@@ -8,6 +8,19 @@ import java.awt.*;
  */
 public class Const {
     public static interface Colors {
-        Color backgroundColor = new Color(240, 246, 255);
+        Color backgroundColor = new Color(228, 250, 255);
+        Color timerFontColor = new Color(200, 73, 46);
+
+        Color markedBtnBgColor = new Color(124, 210, 255);
+        Color unmarkedBtnBgColor = new Color(255, 255, 255);
+    }
+
+    public static interface Fonts {
+        Font textsFont = new Font("SansSerif", Font.PLAIN, 16);
+        Font timerFont = new Font("SansSerif", Font.PLAIN, 48);
+    }
+
+    public static interface Dimensions {
+        Dimension btnSize = new Dimension(250, 60);
     }
 }
