d2b47c456f2c0a73a007e62647c08d149ad51ffd
bakeraw2 <michalskidaniel2@gmail.com>
2013-11-15 22:10:47 +0100

    dodanie odtwarzania filmików

--- src/main/java/media/videos/ver2/Main.java
+++ src/main/java/media/videos/ver2/Main.java
@@ -1,6 +1,4 @@
-package test.main;
-
-import media.videos.ver2.VideoFrame;
+package media.videos.ver2;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
@@ -8,7 +6,7 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
  * Author: Daniel
  * Date: 15.11.13
  */
-public class TestPlayVideo {
+public class Main {
     public static void main(String[] args) {
         VideoFrame videoFrame = new VideoFrame();
         videoFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
