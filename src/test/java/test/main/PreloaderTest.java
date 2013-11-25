d4a55184381ae27c88d83b447e00b964e2800391
bbakeraw2 <mmichalskidaniel2@gmail.com>
2013-11-25 17:45:54 +0100

    preloader

---
+++ src/test/java/test/main/PreloaderTest.java
@@ -0,0 +1,17 @@
+package test.main;
+
+import ui.preloader.PreloaderFrame;
+
+import javax.swing.*;
+
+/**
+ * Author: Daniel
+ * Date: 25.11.13.
+ */
+public class PreloaderTest {
+    public static void main(String[] args) {
+        PreloaderFrame frame = new PreloaderFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
