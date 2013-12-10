dcb0894da2b1ef08e09ca829a1bc7bc5196dff14
bbakeraw2 <mmichalskidaniel2@gmail.com>
2013-12-10 17:13:42 +0100

    dodanie imitacji ładowania danych.

--- src/main/java/RunApplication.java
+++ src/main/java/RunApplication.java
@@ -9,6 +9,24 @@ import java.awt.*;
  */
 public class RunApplication {
     public static void main(String[] args) {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 100; i++) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println(i + " %");
+                }
+            }
+        });
+
+        thread.start();
+
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
---
+++ src/test/java/test/main/MediaTest.java
@@ -0,0 +1,64 @@
+package test.main;
+
+import com.csvreader.CsvReader;
+import encrypt.Encrypter;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static database.columns.QuestionColumnNames.Q_FIRST_MEDIA_PATH;
+import static database.columns.QuestionColumnNames.Q_SECOND_MEDIA_PATH;
+
+/**
+ * Author: Daniel
+ */
+public class MediaTest {
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+
+        InputStream resourceAsStream = MediaTest.class.getResourceAsStream("/csv/questions_enc");
+        byte[] bytesArray = Encrypter.decryptFile(resourceAsStream);
+
+        InputStream byteInputStream = new ByteArrayInputStream(bytesArray);
+        CsvReader csvReader = new CsvReader(byteInputStream, ';', Charset.forName("UTF-8"));
+
+        csvReader.readHeaders();
+
+        int suma = 0;
+        while (csvReader.readRecord()) {
+            try {
+                String mediaPath = null;
+                if (!csvReader.get(Q_FIRST_MEDIA_PATH).equals("")) {
+                    mediaPath = csvReader.get(Q_FIRST_MEDIA_PATH);
+                } else if (!csvReader.get(Q_SECOND_MEDIA_PATH).equals("")) {
+                    mediaPath = csvReader.get(Q_SECOND_MEDIA_PATH);
+                }
+
+                File file = new File("media" + File.separator + mediaPath);
+                if (!file.exists()) {
+                    System.out.println(mediaPath + " nie istnieje");
+                    //continue;
+                }
+
+                suma++;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(suma);
+
+        long end = System.currentTimeMillis();
+        long time = end-start;
+
+        double timeInSec = (double)time / 1000;
+        System.out.println(String.format("Time: %.2f sec.", timeInSec));
+
+    }
+}
