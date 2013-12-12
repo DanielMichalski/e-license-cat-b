533e10f92b26d997ad431ab081270c95c9288e45
bakeraw2 <notarget1>
2013-12-12 12:34:28 +0100

    poprawki

---
+++ src/test/java/database/dao/QuestionDaoTimeTest.java
@@ -0,0 +1,15 @@
+package database.dao;
+
+import database.provider.QuestionsProvider;
+import org.junit.Test;
+
+/**
+ * Author: Daniel
+ */
+public class QuestionDaoTimeTest {
+    @Test (timeout = 1000)
+    public void testGetQuestionsTime() throws Exception {
+        QuestionsProvider questionsProvider =
+                QuestionsProvider.getInstance();
+    }
+}
