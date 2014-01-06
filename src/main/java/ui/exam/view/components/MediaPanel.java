f91104710ef9a3cb23150eeb58df88a9ef828090
bbakeraw2 <mmichalskidaniel2@gmail.com>
2014-01-06 19:32:18 +0100

    poprawki

---
+++ src/main/java/ui/exam/view/components/MediaPanel.java
@@ -0,0 +1,20 @@
+package ui.exam.view.components;
+
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+
+import java.awt.*;
+
+/**
+ * Author: Daniel
+ */
+public class MediaPanel extends EmbeddedMediaPlayerComponent {
+
+    public MediaPanel() {
+        super();
+    }
+
+    @Override
+    public void paintComponents(Graphics g) {
+        super.paintComponents(g);
+    }
+}
